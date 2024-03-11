810681fb83db2e07bc5fb60d3d3626a8369b7aef
Naïké Dramé <naike.drame.dev@gmail.com>
2024-03-11 09:55:10 +0100

    props theory

--- react-the-complete-guide/react-essentials/src/App.jsx
+++ react-the-complete-guide/react-essentials/src/App.jsx
@@ -1,19 +1,44 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    const handleSelect = (e) => {
+        console.log(`You clicked! - selected: ${e.target.textContent}`);
+    };
+
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        {CORE_CONCEPTS.map((concept, index) => (
+                            <CoreConcept
+                                key={index}
+                                title={concept.title}
+                                description={concept.description}
+                                image={concept.image}
+                            />
+                        ))}
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={handleSelect}>
+                            Components
+                        </TabButton>
+                        <TabButton onSelect={handleSelect}>JSX</TabButton>
+                        <TabButton onSelect={handleSelect}>Props</TabButton>
+                        <TabButton onSelect={handleSelect}>State</TabButton>
+                    </menu>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ react-the-complete-guide/react-essentials/src/components/CoreConcept.jsx
@@ -0,0 +1,14 @@
+const CoreConcept = (props) => {
+    return (
+        <li>
+            <img
+                src={props.image}
+                alt={props.title}
+            />
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+        </li>
+    );
+};
+
+export default CoreConcept;
